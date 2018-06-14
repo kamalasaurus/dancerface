34b6d300edd4d385f24bb4fc5aa431d8a0934e10
kamalasaurus <kamalasaurus@gmail.com>
2018-06-14 19:14:35 -0400

    should be the dependencies

---
+++ server.js
@@ -0,0 +1,25 @@
+const {copy, concat, compile} = require('./build.js');
+
+const StaticServer = require('static-server');
+
+
+const scripts = [
+  './node_modules/p5/lib/p5.min.js',
+  './node_modules/@tensorflow/tfjs/dist/ts.min.js',
+  './node_modules/@tensorflow-models/posenet/dist/posenet.min.js'
+]
+.map(copy)
+.reduce(concat, '');
+
+compile(scripts); // build upon launch
+
+
+const server = new StaticServer({
+  rootPath: './app',
+  port: 1337
+});
+
+server.start(() => {
+  console.log('server listening to', server.port);
+});
+
