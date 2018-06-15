f2a8b21e4bc6b98ada85cca647beea056fff0982
kamalasaurus <kamalasaurus@gmail.com>
2018-06-15 13:24:15 -0400

    some words

--- app/main.js
+++ app/main.js
@@ -1,22 +1,23 @@
-//TODO:  video of dancer
-//TODO:  freeze frames of important poses
-//TODO:  run posenet on important poses
-//TODO:  create array of said pose data
-//TODO:  color key for success and failure at each timeline node
-//TODO:  in server.js summon a dominos pizza upon total success!
-//TODO:  block future success so the app doesn't go bankrupt 😅
+//TODO: video of dancer
+//TODO: freeze frames of important poses
+//TODO: run posenet on important poses
+//TODO: create array of said pose data
+//TODO: color key for success and failure at each timeline node
+//TODO: in server.js summon a dominos pizza upon total success!
+//TODO: block future success so the app doesn't go bankrupt 😅
 
-let w = 620
-let h = 480
+const w = 620
+const h = 480
 let video
 let poseNet
 let data = []
+let keyframes = [] // should just be a JSON array that's requested
 let timelinePosition = 0
-let framePositions = [1, 2, 3, 4, 5] // should be millisecond timestamps
+const framePositions = [1, 2, 3, 4, 5] // should be millisecond timestamps
 // only push poses to data when > than millisecond timestamps, ++timelinePosition
 
 function setup() {
-  //TODO:  create video playback element with timeline and pose timestamps highlighted
+  //TODO: create video playback element with timeline and pose timestamps highlighted
   //TODO: video.onend fire reset
   //TODO: video.timestampupdate or whatever to set timeline comparions
 
@@ -25,7 +26,7 @@ function setup() {
   poseNet = ml5.poseNet(
     video,
     'multiple',
-    (results) => { data = results; }
+    (results) => { data = results }
   )
   video.hide()
   fill(255, 0, 0)
@@ -35,7 +36,8 @@ function setup() {
 function draw() {
   image(video, 0, 0, w, h)
   drawData()
-  //TODO:  RMS difference between video feed @time intervals and freeze frames
+  //TODO: RMS difference between video feed @time intervals and freeze frames
+  //reset if too different
 }
 
 function drawData() {
@@ -60,4 +62,5 @@ function timeline() {
 function reset() {
   data = []
   timelinePosition = 0
+  // restart video of dancer
 }
