9409b1c8fe2492647164bc01c8b705b74568b4d8
kamalasaurus <kamalasaurus@gmail.com>
2018-06-19 18:55:58 -0400

    starting to add pizza

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const {copy, concat, compile} = require('./build.js');
-
+const pizzapi = require('dominos');
 const StaticServer = require('static-server');
 
-
+// build app
 const scripts = [
   './node_modules/p5/lib/p5.min.js',
   './node_modules/p5/lib/addons/p5.dom.min.js',
@@ -13,7 +13,36 @@ const scripts = [
 
 compile(scripts); // build upon launch
 
+// setup dominos
 
+let address = new pizzapi.Address('721 Broadway, New York, NY 10003');
+let customer = new pizzapi.Customer({
+  address,
+  firstName: 'NYU',
+  lastName: 'Student',
+  phone: 'xxx-xxx-xxxx',
+  email: 'salami@sandwich.com'
+});
+//TODO: add secrets.json for name and email creds
+
+let myStore;
+
+pizzapi.Util.findNearbyStores(
+  '721 Broadway, New York, NY 10003',
+  'Delivery',
+  (storeData) => {
+    const { result: { Stores: [{ StoreID }] }} = storeData;
+    myStore = new pizzapi.Store();
+    myStore.ID = StoreID;
+  }
+);
+
+//get menu
+//get code for cheese pizza
+//make order for cheese pizza
+
+
+// run server
 const server = new StaticServer({
   rootPath: './app',
   port: 1337
