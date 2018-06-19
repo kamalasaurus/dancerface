50f6bb2b0ec5d8da7d28c83872a2722a86ca041a
kamalasaurus <kamalasaurus@gmail.com>
2018-06-19 18:28:19 -0400

    finished rms

--- app/main.js
+++ app/main.js
@@ -2,20 +2,21 @@
 //TODO: freeze frames of important poses
 //TODO: run posenet on important poses
 //TODO: create array of said pose data
-//TODO: color key for success and failure at each timeline node
 //TODO: in server.js summon a dominos pizza upon total success!
 //TODO: block future success so the app doesn't go bankrupt 😅
 
-const w = 620
-const h = 480
-let video
-let dancing
+const threshold = 20;
 
-let poseNet
-let data = []
-let keyframes = [] // should just be a JSON array that's requested
-let timelinePosition = 0
-const framePositions = [1, 2, 3, 4, 5] // should be millisecond timestamps
+const w = 620;
+const h = 480;
+let video;
+let dancing;
+
+let poseNet;
+let data = [];
+let keyframes = []; // should just be JSON array of keypoints that are precomputed for poses in timestamp order!
+let currentPosition = 0;
+const framePositions = [1, 2, 3, 4, 5]; // should be millisecond timestamps
 // only push poses to data when > than millisecond timestamps, ++timelinePosition
 
 function setup() {
@@ -23,14 +24,12 @@ function setup() {
   //TODO: video.onend fire reset
   //TODO: video.timestampupdate or whatever to set timeline comparions
 
-  createCanvas(w, h)
+  createCanvas(w, h);
   //video = createCapture(VIDEO)
 
   //dancing = createVideo('./assets/flossing.mp4', (vid) => { vid.play() })
 
-  dancing = createVideo('https://media.giphy.com/media/xUA7aXRRUlmqhoG7q8/giphy.mp4', () => { dancing.play() })
-
-  debugger;
+  dancing = createVideo('https://media.giphy.com/media/xUA7aXRRUlmqhoG7q8/giphy.mp4', () => { dancing.play() });
 
   //poseNet = ml5.poseNet(
     //video,
@@ -44,39 +43,65 @@ function setup() {
     dancing,
     'multiple',
     (results) => { data = results }
-  )
-  dancing.hide()
+  );
+  dancing.hide();
 
-  fill(255, 0, 0)
-  stroke(255, 0, 0)
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
 }
 
 function draw() {
-  //image(video, 0, 0, w, h)
-  image(dancing, 0, 0, dancing.width, dancing.height)
-  drawData()
+  //image(video, 0, 0, w, h);
+  image(dancing, 0, 0, dancing.width, dancing.height);
+  drawData();
   //TODO: RMS difference between video feed @time intervals and freeze frames
   //reset if too different
 }
 
 function drawData() {
-  console.log(data)
+  console.log(data);
   data.forEach((datum) => {
 
     datum.pose.keypoints.forEach((keypoint) => {
       if (keypoint.score > 0.2)
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10)
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
     });
 
     datum.skeleton.forEach(([start, end]) => {
-      line(start.position.x, start.position.y, end.position.x, end.position.y)
+      line(start.position.x, start.position.y, end.position.x, end.position.y);
     });
+
+    //compareKeypoints(datum.pose.keypoints)
 
   });
 }
 
-function timeline() {
+function compareKeypoints(livePose) {
+  let expectedPose = keyFrames[currentPosition]
+  let pointsByKey = livePose.concat(expectedPose)
+    .reduce((obj, point) => {
+      const { part, position } = point;
+      obj[part] = obj[part] ?
+        obj[part].concat(position) :
+        [ position ];
+      return obj;
+    }, {});
 
+  return Object.keys(pointsByKey)
+    .map(key => distance(pointsByKey[key]))
+    .every(el => el < threshold);
+}
+
+function distance(pointarray) {
+  const [
+    {x: x1, y: y1},
+    {x: x2 = x1, y: y2 = y1}
+  ] = pointarray;
+  return [rms(x1, x2), rms(y1, y2)]
+}
+
+function rms(arg1, arg2) {
+  return Math.pow(Math.pow(arg1, 2) - Math.pow(arg2, 2), 0.5);
 }
 
 function reset() {
